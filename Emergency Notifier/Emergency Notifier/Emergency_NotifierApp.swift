16b1fa63c9a478261bb5d5034a549670b1dd494b
Adnan <Adnanodima1@Gmail.com>
2021-12-18 16:55:03 +0000

    Start
    Added Files from xcode

---
+++ Emergency Notifier/Emergency Notifier/Emergency_NotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emergency_NotifierApp.swift
+//  Emergency Notifier
+//
+//  Created by Adnan Odimah on 18/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Emergency_NotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
