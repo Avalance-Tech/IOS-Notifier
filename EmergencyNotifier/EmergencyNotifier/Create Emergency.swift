c0311ae502f7e8768bc1bdaadbf1988015df8194
Adnan-Odimah <adnan11odimah@gmail.com>
2022-01-17 07:20:55 +0000

    started work on sorts

--- EmergencyNotifier/EmergencyNotifier/Create Emergency.swift
+++ EmergencyNotifier/EmergencyNotifier/Create Emergency.swift
@@ -88,22 +88,45 @@ struct showSelectedEmergency: View{
 
 
 struct SortsView: View{
+
+    @Binding var sort: String
+    @Binding var sortOrder: Bool
     
-    // var vm: ArrayModification
     
     var descendingImage = "chevron.down"
     var ascendingImage = "chevron.up"
     
+    let sorts = ["Id", "Name", "Status", "Branch"]
+    
     var body: some View{
         
         Button {
-            
+
+            sort = "Name"
+            sortOrder.toggle()
+
         } label: {
-            Text("Name")
+            HStack{
+
+                Text("Name")
+                Image(systemName: sortOrder && sort == "Name" ? "chevron.down" : "chevron.up")
+
+            }
         }
         
-        Text("Sorts")
-        
+        Button {
+
+            sort = "Id"
+            sortOrder.toggle()
+
+        } label: {
+            HStack{
+
+                Text("Employee ID")
+                Image(systemName: sortOrder && sort == "Id" ? "chevron.down" : "chevron.up")
+
+            }
+        }
         
         
     }
@@ -111,8 +134,12 @@ struct SortsView: View{
 
 
 struct FiltersView: View{
+    @Binding var filters: [String]
+    
     
     var body: some View{
+        
+        
         Text("Filters")
         
         
@@ -122,71 +149,29 @@ struct FiltersView: View{
 
 
 
-
-
-class ArrayModification: ObservableObject{
-    
-    var sortType: String   =  ""
-    
-    @Published var dataArray:  [Employee] = []
-    @Published var filteredArray: [Employee] = []
-    
-    init(){
-        
-        getEmployees()
-        updateFilteredArray()
-        
-    }
-    
-    func updateFilteredArray(){
-        
-        // sort
-        if sortType == "" || sortType == "EmployeeID"{
-            filteredArray = dataArray.sorted(by: { $0.employeeID > $1.employeeID })
-        }
-        else if sortType == "Name"{
-            filteredArray = dataArray.sorted(by: {$0.name > $1.name})
-        }
-        /*
-         else if sortType == "Status"{
-         filteredArray = dataArray.sorted(by: {
-         $0.status == true || $1.status == false
-         return 0
-         })
-         */
-    }
-    
-    // filter
-    
-    
-    
-    func getEmployees(){
-        self.dataArray.append(contentsOf: x.allEmployees)
-    }
-    
-    
-}
-
 struct Create_Emergency: View {
     
     // Sort / filter employees shown
     
-    @StateObject var vm = ArrayModification()
-    
-    
     var allEmployeesShown: Array<Employee>{
-        
         return [adnan, talal, wassim, ayman]
-        
     }
+    
+    @State var filters: [String] = []
     
     var shownEmployees: [Employee]{
-        return self.allEmployeesShown
+        var emp: [Employee] = []
+        
+        emp = doSort(list: allEmployeesShown,  sort: sort, type: typeS)
+        
+        
+        
+        return emp
     }
     
+    @State var typeS = true
     @State var search = ""
-    @State var filters = ""
-    @State var sort = ""
+    @State var sort = "Status"
     
     @State var showFilters = false
     @State var showSorts = false
@@ -200,7 +185,10 @@ struct Create_Emergency: View {
     @State var emergencyDate: Date = Date()
     
     
+    
+    
     @State var selectedEmployeesID = Set<UUID>()
+    
     var selectedEmployees: [Employee]{
         
         var employees: [Employee] = []
@@ -223,48 +211,58 @@ struct Create_Emergency: View {
         
         VStack(spacing: 15){
             
-            
-            // Emergency Details
-            HStack(spacing: 10){
-                Text("Details")
-                TextField(" Emergency Details", text: $emergencyDetails)
-                    .background(Color.gray.opacity(0.1).cornerRadius(10))
-                
-            }
-            .padding(.horizontal, 10)
-            
-            //Emergency Location
-            HStack(spacing: 10){
-                Text("Location")
-                TextField("Emergency Location", text: $emergencyLocation)
-                    .background(Color.gray.opacity(0.1).cornerRadius(10))
-            }
-            .padding(.horizontal, 10)
-            
-            
-            //Meeting point Location
-            HStack(spacing: 10){
-                Text("Meeting Location")
-                TextField("Meeting Point Location", text: $meetingPoint)
-                    .background(Color.gray.opacity(0.1).cornerRadius(10))
-            }
-            .padding(.horizontal, 10)
-            
-            
-            HStack(spacing: 10){
-                Stepper(value: $emergencyUrgency, in: 1...5) {
-                    Text("Urgency:")
-                    Text(String(emergencyUrgency))
+            if !showSorts && !showFilters{
+                // Emergency Details
+                HStack(spacing: 10){
+                    Text("Details")
+                    TextField(" Emergency Details", text: $emergencyDetails)
+                        .background(Color.gray.opacity(0.1).cornerRadius(10))
+                    
                 }
+                .padding(.horizontal, 10)
+                
+                //Emergency Location
+                HStack(spacing: 10){
+                    Text("Location")
+                    TextField("Emergency Location", text: $emergencyLocation)
+                        .background(Color.gray.opacity(0.1).cornerRadius(10))
+                }
+                .padding(.horizontal, 10)
+                
+                
+                //Meeting point Location
+                HStack(spacing: 10){
+                    Text("Meeting Location")
+                    TextField("Meeting Point Location", text: $meetingPoint)
+                        .background(Color.gray.opacity(0.1).cornerRadius(10))
+                }
+                .padding(.horizontal, 10)
+                
+                
+                HStack(spacing: 10){
+                    Stepper(value: $emergencyUrgency, in: 1...5) {
+                        Text("Urgency:")
+                        Text(String(emergencyUrgency))
+                    }
+                }
+                .padding(.horizontal, 10)
+                
+                HStack(spacing: 10){
+                    
+                    DatePicker(selection: $emergencyDate, label: { Text("Time") })
+                    
+                }
+                .padding(.horizontal, 10)
+                
             }
-            .padding(.horizontal, 10)
             
-            HStack(spacing: 10){
+            else if showSorts{
+     
+                SortsView(sort: $sort, sortOrder: $typeS)
                 
-                DatePicker(selection: $emergencyDate, label: { Text("Time") })
+            } else if showFilters{
                 
             }
-            .padding(.horizontal, 10)
             
             
             Divider().onTapGesture {
@@ -296,7 +294,7 @@ struct Create_Emergency: View {
                 }.offset(x:-6)
                     .frame(width: UIScreen.main.bounds.width + 10)
                 
-                ForEach(vm.filteredArray) {Employee in
+                ForEach(shownEmployees) {Employee in
                     
                     showSelectedEmergency(employee: Employee, selectedItems: $selectedEmployeesID)
                     
@@ -317,7 +315,7 @@ struct Create_Emergency: View {
                     print(meetingPoint)
                     print(emergencyUrgency)
                     
-                    print(shownEmployees)
+                    print(selectedEmployees)
                     
                     
                 }, label:
@@ -334,14 +332,40 @@ struct Create_Emergency: View {
                 
                 
             }.padding(.all, 5)
-                .popover(isPresented: $showFilters, content: {
-                    FiltersView()
-                })
-                .popover(isPresented: $showSorts) {
-                    SortsView()
-                }
         }
     }
+    
+    
+    
+    
+    
+    
+    func doSort(list: Array<Employee>, sort: String, type: Bool) -> [Employee]{
+        
+        // if type true then desc else asc
+        
+        var newList: [Employee] = []
+        
+        if sort == "Name"{
+            if type{
+                newList = list.sorted(by: {$0.name < $1.name})
+            }
+            else{
+                newList = list.sorted(by: {$0.name > $1.name})
+            }
+            
+        }else if sort == "Id"{
+            if type{
+                newList = list.sorted(by: {$0.employeeID < $1.employeeID})
+            }
+            else{
+                newList = list.sorted(by: {$0.employeeID > $1.employeeID})
+            }
+        }
+        return newList
+    }
+    
+    
     
     
 }
