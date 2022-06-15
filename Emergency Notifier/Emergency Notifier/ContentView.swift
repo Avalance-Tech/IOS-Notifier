1796571847887540e5d2780d4e7fd14cddd2bed5
Adnan <adnan11odimah@gmail.com>
2022-06-15 08:49:17 +0100

    created main files

---
+++ Emergency Notifier/Emergency Notifier/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Emergency Notifier
+//
+//  Created by Adnan Odimah on 15/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Emergency Notifier/Emergency Notifier/Emergency_NotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emergency_NotifierApp.swift
+//  Emergency Notifier
+//
+//  Created by Adnan Odimah on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Emergency_NotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
